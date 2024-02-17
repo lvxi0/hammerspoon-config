df0df04e393e3b207c0da90198267135d1c95680
lvxi0 <xi.lyu@icloud.com>
2024-02-17 22:31:33 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,159 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+  end)
+
+hs.loadSpoon("AClock")
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "C", function()
+    spoon.AClock:toggleShow()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Y", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  f.y = f.y - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
+  f.y = f.y + 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.y = max.y
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.y = max.y + (max.h / 2)
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- 定义切换应用程序函数
+function toggleApplication(name)
+    return function()
+        local app = hs.application.get(name)
+        if not app or not app:mainWindow() then
+            hs.application.launchOrFocus(name)
+        elseif hs.window.visibleWindows() then
+            hs.window.filter.new{name}:getWindows(hs.window.filter.sortByFocusedLast)[1]:focus()
+        end
+    end
+end
+
+-- 设置快捷键来切换应用程序
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "1", toggleApplication("Google Chrome"))
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "2", toggleApplication("Alacritty"))
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "3", toggleApplication("Safari"))
+-- 可以根据需要添加更多应用程序和相应的快捷键
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
